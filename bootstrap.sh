c6599b4c6835eea6f85b72820b7fef6cf69863a9
Masaya Ozawa <ma3ya.ozw+github@gmail.com>
2013-09-02 07:08:08 +0000

    update bootstrap.php

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 DIR=`pwd`
 
-[[ -f ~/.bash_profile ]] || ln -s ${DIR}/.bashrc    ~/.bashrc
-[[ -f ~/.bashrc ]]       || ln -s ${DIR}/.bashrc    ~/.bashrc
-[[ -f ~/.bash_ps ]]      || ln -s ${DIR}/.bash_ps   ~/.bash_ps
-[[ -f ~/.vimrc ]]        || ln -s ${DIR}/.vimrc     ~/.vimrc
-[[ -f ~/.screenrc ]]     || ln -s ${DIR}/.screenrc  ~/.screenrc
-[[ -f ~/.gitconfig ]]    || ln -s ${DIR}/.gitconfig ~/.gitconfig
+[[ -f ~/.bash_profile ]] || ln -s ${DIR}/.bash_profile ~/.bash_profile
+[[ -f ~/.bashrc ]]       || ln -s ${DIR}/.bashrc       ~/.bashrc
+[[ -f ~/.bash_ps ]]      || ln -s ${DIR}/.bash_ps      ~/.bash_ps
+[[ -f ~/.vimrc ]]        || ln -s ${DIR}/.vimrc        ~/.vimrc
+[[ -f ~/.screenrc ]]     || ln -s ${DIR}/.screenrc     ~/.screenrc
+[[ -f ~/.gitconfig ]]    || ln -s ${DIR}/.gitconfig    ~/.gitconfig
 
 . ~/.bashrc
 
