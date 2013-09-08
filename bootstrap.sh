e1b90c70238b6b48659494a3226b8ba4bb54987c
Masaya Ozawa <ma3ya.ozw+github@gmail.com>
2013-09-08 01:19:02 +0000

    .tmux.confの反映処理を追加

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,9 @@ DIR=`pwd`
 [[ -f ~/.bashrc ]]       || ln -s ${DIR}/.bashrc       ~/.bashrc
 [[ -f ~/.bash_ps ]]      || ln -s ${DIR}/.bash_ps      ~/.bash_ps
 [[ -f ~/.vimrc ]]        || ln -s ${DIR}/.vimrc        ~/.vimrc
-[[ -f ~/.screenrc ]]     || ln -s ${DIR}/.screenrc     ~/.screenrc
 [[ -f ~/.gitconfig ]]    || ln -s ${DIR}/.gitconfig    ~/.gitconfig
+[[ -f ~/.tmux.conf ]]    || ln -s ${DIR}/.tmux.conf    ~/.tmux.conf
+[[ -f ~/.screenrc ]]     || ln -s ${DIR}/.screenrc     ~/.screenrc
 
 # neobundleの導入
 # ---------------
