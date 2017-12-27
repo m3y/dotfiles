e2491d38d21af728c1a58cfe78a0f2098726c7f5
m3y <ma3ya.ozw+github@gmail.com>
2017-12-27 22:35:10 +0900

    微修正

--- .bin/update.sh
+++ .bin/update.sh
@@ -4,5 +4,5 @@ brew update
 brew upgrade
 brew cask cleanup
 for c in `brew cask list`; do
-    ! brew cask info $c | grep -qF "Not installed" || brew cask install $c; 
+    ! brew cask info $c | grep -qF "Not installed" || brew cask install $c;
 done
